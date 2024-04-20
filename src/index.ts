b52bb396395b3f49d825475c8099ed11adfb845e
Henry Paulino <corasan.dev@gmail.com>
2024-04-20 14:31:52 +0100

    v2.0.0-rc.1 (#8)
    * Refactor ModalSheet component and remove unused code in ModalSheet.tsx
    
    * Refactor ModalSheet component and update imports in index.ts
    
    * Update version to 2.0.0-rc.1 in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export * from './Providers/ModalSheetProvider'
-export * from './ModalSheet'
+import { ModalSheetProvider } from './Providers/ModalSheetProvider'
+import { ModalSheet } from './ModalSheet'
+import { ModalSheetProps, ModalSheetRef } from './types'
+
+export { ModalSheetRef, ModalSheetProps, ModalSheet, ModalSheetProvider }
