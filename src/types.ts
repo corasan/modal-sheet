90c2cfcb9a3111e3eb0c81445119ad9e113df2fa
Henry Paulino <corasan.dev@gmail.com>
2024-05-22 10:13:19 -0400

    chore: Update package-lock.json and ModalSheet components

--- src/types.ts
+++ src/types.ts
@@ -37,7 +37,6 @@ export interface ModalSheetRef {
   minimizedHeight?: number
   id: string
   children?: React.ReactNode
-  // onLayoutChange: (height: number) => void
 }
 export interface ModalSheetProps {
   name: string
